ad4df6caaa0951a7debb9bcadf7e155c3ea7b79b
Ltfjx <ltfjx2333@gmail.com>
2022-07-21 20:04:32 +0800

    rua

--- md2wp.py
+++ md2wp.py
@@ -6,18 +6,19 @@ md = md.splitlines()
 o = open("./output.txt", "w+", encoding='utf-8')
 
 inCodeBlock=False
+inUList=False
+inOList=False
 
 for i in range(len(md)):
 
-    print(md[i])
+    #print(md[i])
 
     line = md[i].split(" ")
 
     if (md[i] == ""): continue
 
-
-    # ========代码块========
-
+    # ========特殊块========
+    
     if(inCodeBlock):
         if(md[i][0:3]=="```"): # 新行```结束代码块
             utils.writeCodeBlockEnd(o)
@@ -33,17 +34,46 @@ for i in range(len(md)):
         o.write("\n")
         continue
 
+
+    if(inUList and (md[i].find("-",0,2)!=-1 and md[i].count("-",0,2)==1)):
+        utils.writeUListItem(o,md[i].lstrip(" -"))
+        continue
+    if (inUList):
+        utils.writeUListEnd(o)
+        inUList=False
+        
+
+    # ========特殊块结束========
+
+    # ========图片========
+    if(md[i][0:2]=="!["):
+        utils.writePic(o)
+        continue
+
+    # ========图片结束========
+
+    # ========单行代码块========
+    if(md[i] != "```" and md[i][0] == "`" and md[i][-1] == "`"):
+        utils.writeCodeBlockStart(o,"")
+        o.write(md[i].strip("`"))
+        o.write("\n")
+        utils.writeCodeBlockEnd(o)
+        continue
+
+
+    # ========单行代码块结束========
+
+    # ========代码块========
+
+
+
     if(md[i][0:3]=="```"):
         inCodeBlock = True
         CodeBlock_Language = md[i].lstrip("`")
         utils.writeCodeBlockStart(o,CodeBlock_Language)
         continue
-    
+
     # ========代码块结束========
-
-
-
-
 
     # ========标题========
     # H1
@@ -78,10 +108,90 @@ for i in range(len(md)):
         continue
     # ========标题结束========
 
+    # ========分割线========
+    if(md[i]=="---" or md[i]=="-----" or md[i]=="***" or md[i]=="*****"):
+        utils.writeDivideLine(o)
+        continue
+    # ========分割线结束========
+
+
+    # ========无序列表========
+    
+    
+    if(md[i].find("-",0,2)!=-1 and md[i].count("-",0,2)==1):
+        inUList = True
+        utils.writeUListStart(o)
+        utils.writeUListItem(o,md[i].lstrip(" -"))
+        continue
+
+    # ========无序列表结束========
+
+    # ========行内样式========
+    if(md[i].find("*") !=-1 or md[i].find("~~")!=-1 or md[i].find("_") !=-1):
+        text=""
+
+        if ((md[i].count("***"))%2 == 0):
+            while md[i].count("***") != 0:
+                md[i] = md[i].replace("***","<strong><em>",1)
+                md[i] = md[i].replace("***","</em></strong>",1)
+
+        if ((md[i].count("**"))%2 == 0):
+            while md[i].count("**") != 0:
+                md[i] = md[i].replace("**","<strong>",1)
+                md[i] = md[i].replace("**","</strong>",1)
+
+        if ((md[i].count("*"))%2 == 0):
+            while md[i].count("*") != 0:
+                md[i] = md[i].replace("*","<em>",1)
+                md[i] = md[i].replace("*","</em>",1)
+            
+        if ((md[i].count("~~"))%2 == 0):
+            while md[i].count("~~") != 0:
+                md[i] = md[i].replace("~~","<s>",1)
+                md[i] = md[i].replace("~~","</s>",1)
+
+
+        utils.writeText(o,md[i])
+        continue
+
+
+
+
+
+        # temp = md[i]
+        # while temp.find("***")!= -1:
+        #     start = temp.find("***")
+        #     end = temp.find("***",start+1)
+        #     text = text + utils.getBoldEmText(temp[start+3:end])
+        #     temp = temp[(end+3):]
+
+
+        # temp = md[i]
+        # while temp[i].find("**")!= -1:
+        #     start = md[i].find("**")
+        #     end = md[i].find("**",start+1)
+        #     text = text + utils.getBoldText(md[i][start+2:end])
+        #     temp = temp[(end+2):]
+
+
+        # temp = md[i]
+        # while temp[i].find("*")!= -1:
+        #     start = md[i].find("*")
+        #     end = md[i].find("*",start+1)
+        #     text = text + utils.getEmText(md[i][start+1:end])
+        #     temp = temp[(end+1):]
+        # print(text)
+        # continue
+
+
+
+
+    # ========行内样式结束========
+
     utils.writeText(o,md[i])
 
 
 
-    ##for ii in range(len(line)):
+    #for ii in range(len(line)):
     #    if 
-    ##   else
+    #   else
--- md2wp/utils.py
+++ md2wp/utils.py
@@ -29,3 +29,37 @@ def writeCodeBlockEnd(file):
     o.write("</code></pre>\n")
     o.write("<!-- /wp:code -->\n")
     o.write("\n")
+
+
+def writeDivideLine(file):
+    o = file
+    o.write("<!-- wp:separator -->\n")
+    o.write("<hr class=\"wp-block-separator has-alpha-channel-opacity\"/>\n")
+    o.write("<!-- /wp:separator -->\n")
+    o.write("\n")
+
+
+def writeUListStart(file):
+    o = file
+    o.write("<!-- wp:list -->\n")
+    o.write("<ul>")
+
+    
+def writeUListEnd(file):
+    o = file
+    o.write("</ul>\n")
+    o.write("<!-- /wp:list -->\n")
+    o.write("\n")
+
+def writeUListItem(file,text):
+    o=file
+    o.write("<li>")
+    o.write(text)
+    o.write("</li>")
+
+def writePic(file):
+    o=file
+    o.write("<!-- wp:image -->\n")
+    o.write("<figure class=\"wp-block-image\"><img alt=""/></figure>\n")
+    o.write("<!-- /wp:image -->\n")
+    o.write("\n")
